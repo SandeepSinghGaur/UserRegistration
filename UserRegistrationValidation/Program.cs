ae211507cb2aea50f9477aae28e1d722db807ebf
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-02-04 15:28:22 +0530

    [Sandeep] Added:Validate Last Name Of User Registration

--- UserRegistrationValidation/Program.cs
+++ UserRegistrationValidation/Program.cs
@@ -7,9 +7,15 @@ namespace UserRegistrationValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration");
-            Console.WriteLine("Enter First Name :");
-            string first_name = Console.ReadLine();
-            RegexValidation.FirstNameMatch(first_name);
+            //Console.WriteLine("Enter First Name :");
+            //string first_name = Console.ReadLine();
+            //RegexValidation.FirstNameMatch(first_name);
+            Console.WriteLine("Enter Last Name :");
+            string last_name = Console.ReadLine();
+            RegexValidation.LastNameMatch(last_name);
+
         }
+
+
     }
 }
--- UserRegistrationValidation/RegexValidation.cs
+++ UserRegistrationValidation/RegexValidation.cs
@@ -17,5 +17,14 @@ namespace UserRegistrationValidation
             else
                 Console.WriteLine("No ! First Name is not Validate");
         }
+        public static string LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static void LastNameMatch(string last_name)
+        {
+            bool result = Regex.IsMatch(last_name, LAST_NAME);
+            if (result == true)
+                Console.WriteLine("Yes ! Last Name is Validate");
+            else
+                Console.WriteLine("No ! Last Name is not Validate");
+        }
     }
 }
