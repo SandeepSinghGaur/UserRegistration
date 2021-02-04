4f2a4dfc87794be845ade61195375015fb68e090
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-02-04 15:21:41 +0530

    [Sandeep] Added:Validate First Name Of User Registration

---
+++ UserRegistrationValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistrationValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("Enter First Name :");
+            string first_name = Console.ReadLine();
+            RegexValidation.FirstNameMatch(first_name);
+        }
+    }
+}
---
+++ UserRegistrationValidation/RegexValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationValidation
+{
+    class RegexValidation
+    {
+
+        public static string FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static void FirstNameMatch(string first_name)
+        {
+            bool result = Regex.IsMatch(first_name, FIRST_NAME);
+            if (result)
+                Console.WriteLine("Yes ! First Name is Validate");
+            else
+                Console.WriteLine("No ! First Name is not Validate");
+        }
+    }
+}
